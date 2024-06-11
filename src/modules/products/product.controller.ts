f38b808b80e69892c400ed35d519dff55c08a761
Abdiel49 <abdielorellana3@gmail.com>
2024-06-10 22:11:00 -0400

    update try-catch on controllers

--- src/modules/products/product.controller.ts
+++ src/modules/products/product.controller.ts
@@ -2,61 +2,96 @@ import {Request, Response, response} from 'express';
 import ProductService from './products.service';
 
 async function getAllProducts (_req: Request, res: Response) {
-  const products = await ProductService.getProducts()
-  return res.json(products);
+  try {
+    const products = await ProductService.getProducts()
+    return res.json(products);
+  } catch (error) {
+    console.error('Error getting products');
+    return res.status(500).send({
+      "message": "error getting products"
+    })
+  }
 }
 
 async function getProductById (req: Request, res: Response) {
-  const productId = req.params.id
+  try {
+    const productId = req.params.id
+    
+    const product = await ProductService.getById(+productId)
+    if (!product) {
+      return res.status(404).send({
+        "message": "product not found"
+      }) 
+    }
   
-  const product = await ProductService.getById(+productId)
-  if (!product) {
-    return res.status(404).send({
-      "message": "product not found"
-    }) 
+    return res.json(product);
+  } catch (error) {
+    console.error('error getting product by id', error);
+    return res.status(500).send({
+      "message": "error getting product by id"
+    })
   }
-
-  return res.json(product);
 }
 
 async function createProduct (req: Request, res: Response) {
-  const rawProduct = req.body;
-  // validate product
-
-  // save product
-  const product = await ProductService.createProduct(rawProduct)
-  return res.status(201).json(product)
+  try {
+    const rawProduct = req.body;
+    // validate product
+  
+    // save product
+    const product = await ProductService.createProduct(rawProduct)
+    return res.status(201).json(product)
+  } catch (error) {
+    console.error('error creating product', error);
+    return res.status(500).send({
+      "message": "error creating product"
+    })
+  }
 }
 
 async function updateProduct (req: Request, res: Response) {
-  const productId = req.params.id
-  const rawProduct = req.body
-  // validate product
-  console.log('product to update', rawProduct);
-
-  let product = await ProductService.getById(+productId)
-  if (!product) {
-    return res.status(404).send({
-      "message": "product not found"
-    }) 
+  try {
+    const productId = req.params.id
+    const rawProduct = req.body
+    // validate product
+    console.log('product to update', rawProduct);
+  
+    let product = await ProductService.getById(+productId)
+    if (!product) {
+      return res.status(404).send({
+        "message": "product not found"
+      }) 
+    }
+  
+    const response =  await ProductService.updateProduct(+productId, rawProduct)
+    return res.json(response)
+  } catch (error) {
+    console.error('Error on update product', error);
+    return res.status(500).send({
+      "message": "error on update product"
+    })
   }
-
-  const response =  await ProductService.updateProduct(+productId, rawProduct)
-  return res.json(response)
 }
 
 async function deleteProduct (req: Request, res: Response) {
-  const productId = req.params.id
-
-  let product = await ProductService.getById(+productId)
-  if (!product) {
-    return res.status(404).send({
-      "message": "product not found"
-    }) 
+  try {
+    const productId = req.params.id
+  
+    let product = await ProductService.getById(+productId)
+    if (!product) {
+      return res.status(404).send({
+        "message": "product not found"
+      }) 
+    }
+  
+    const response = await ProductService.deleteProduct(+productId)
+    return res.json(response)
+  } catch (error) {
+    console.error('Error on delete product', error);
+    return res.status(500).send({
+      "message": "error on delete product"
+    })
   }
-
-  const response = await ProductService.deleteProduct(+productId)
-  return res.json(response)
 }
 
 export default {
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,73 +1,108 @@
-import {Request, Response} from 'express'
+import e, {Request, Response} from 'express'
 import UserService from './users.service';
 
 async function getAllUsers (_req: Request, res: Response) {
-  const users = await UserService.getAllUsers()
-  return res.json(users);
+  try {
+    const users = await UserService.getAllUsers()
+    return res.json(users);
+  } catch (error) {
+    console.error('Error getting users', error);
+    return res.status(500).send({
+      "message": "error getting users"
+    })
+  }
 }
 
 async function getUserById (req: Request, res: Response) {
-  // estraer los datos de la request
-  const userId = req.params.id
-  // buscar los datos
-  // const user = usersData.find(user => user.id === userId)
-  const user = await UserService.getById(+userId)
-
-  // responder con el usuario
-  if (!user) {
-    return res.status(404).send({
-      "message": "user not found"
-    }) 
+  try {
+    // extraer los datos de la request
+    const userId = req.params.id
+    // buscar los datos
+    // const user = usersData.find(user => user.id === userId)
+    const user = await UserService.getById(+userId)
+  
+    // responder con el usuario
+    if (!user) {
+      return res.status(404).send({
+        "message": "user not found"
+      }) 
+    }
+  
+    return res.json(user)
+  } catch (error) {
+    console.error('Failed to get user');
+    return res.status(500).send({
+      "message": "failed to get user"
+    })
   }
-
-  return res.json(user)
 }
 
 async function createUser (req: Request, res: Response) {
-  // estraer los datos de la request
-  const bodyData = req.body;
-  console.log('bodyData', bodyData);
-  // validar los datos del usuario
-  const user = await UserService.createUser(bodyData);
-  // responder con el usuario creado satisfied
-  return res.status(201).json(user)
+  try {
+    // estraer los datos de la request
+    const bodyData = req.body;
+    console.log('bodyData', bodyData);
+    // validar los datos del usuario
+    const user = await UserService.createUser(bodyData);
+    // responder con el usuario creado satisfied
+    return res.status(201).json(user)
+  } catch (error) {
+    console.error('Error on creare user', error);
+    return res.status(500).send({
+      "message": "error on creare user"
+    })
+  }
 }
 
 async function updateUser (req: Request, res: Response) {
-  // estraer los datos de la request
-  const userId = req.params.id
-  const bodyData = req.body;
-
-  // TODO: validar los datos del usuario
-  // validar que el usuario con el ID existe
-  let user = await UserService.getById(+userId)
-  if (!user) {
-    return res.status(404).send({
-      "message": "user not found"
-    }) 
+  try {
+    // estraer los datos de la request
+    const userId = req.params.id
+    const bodyData = req.body;
+  
+    // TODO: validar los datos del usuario
+    // validar que el usuario con el ID existe
+    let user = await UserService.getById(+userId)
+    if (!user) {
+      return res.status(404).send({
+        "message": "user not found"
+      }) 
+    }
+  
+    const response = await UserService.updateUser(+userId, bodyData);
+    return res.status(200).json(response)
+  } catch (error) {
+    console.error("Error updating user", error);
+    return res.status(500).send({
+      "message": "error updating user"
+    })
   }
-
-  const response = await UserService.updateUser(+userId, bodyData);
-  return res.status(200).json(response)
 }
 
 async function deleteUser (req: Request, res: Response) {
-  // estraer los datos de la request
-  const userId = req.params.id
-
-  // validar que el usuario con el ID existe
-  let user = UserService.getById(+userId)
-  if (!user) {
-    return res.status(404).send({
-      "message": "user not found"
-    }) 
+  try {
+    // estraer los datos de la request
+    const userId = req.params.id
+  
+    // validar que el usuario con el ID existe
+    let user = UserService.getById(+userId)
+    if (!user) {
+      return res.status(404).send({
+        "message": "user not found"
+      }) 
+    }
+  
+    // eliminar el usuario
+    const deleted = await UserService.deleteUser(userId)
+  
+    // responder con el usuario creado satisfied
+    return res.status(200).json(deleted)
+  } catch (error) {
+    console.error("Error on delete user", error);
+    return res.status(500).send({
+      "message": "error on delete user"
+    })
   }
-
-  // eliminar el usuario
-  const deleted = await UserService.deleteUser(userId)
-
-  // responder con el usuario creado satisfied
-  return res.status(200).json(deleted)
 }
 
 export default {
